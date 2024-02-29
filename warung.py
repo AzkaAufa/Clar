908c382174eb80a732bb6df401aa9028be41476a
AzkaAufa <160815115+AzkaAufa@users.noreply.github.com>
2024-02-29 11:27:50 +0700

    Add files via upload

---
+++ warung.py
@@ -0,0 +1,12 @@
+rokok = 20000
+minyak = 25000
+gula = 11000
+diskon = 10000
+saldo = 100000
+
+hasil = rokok + minyak + gula
+potongan_diskon = hasil - diskon
+kembalian = saldo - potongan_diskon
+print('Total nya:', hasil)
+print('Setelah mendapat diskon', potongan_diskon)
+print ('jadi total nya:', kembalian)
